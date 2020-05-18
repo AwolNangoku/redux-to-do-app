bcf9fb164f9dd63028b987cb3ecba307eec5e00b
ncedile <ncedile@yoco.co.za>
2020-05-18 10:51:44 +0200

    Code cleanup

--- src/services/state/todos/reducer.js
+++ src/services/state/todos/reducer.js
@@ -6,10 +6,7 @@ const initialState = [];
 const todos = createReducer(initialState,
 {
 	[actions.listTodos]: (state, action) => (state),
-	[actions.addToDo]: (state, action) => {
-		console.log('11111', action.payload)
-		return [...state, action.payload];
-	},
+	[actions.addToDo]: (state, action) => ([...state, action.payload]),
 	[actions.deleteToDo]: (state, action) => (state.filter(todo => todo !== action.payload))
 },
 initialState
